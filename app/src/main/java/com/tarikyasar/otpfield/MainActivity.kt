4f6448e0d214a612576f9590e6d6a943f10dc199
Tarik Yasar <m.tarikyasar@gmail.com>
2023-07-21 18:25:04 +0300

    Add basic otp implementation

--- app/src/main/java/com/tarikyasar/otpfield/MainActivity.kt
+++ app/src/main/java/com/tarikyasar/otpfield/MainActivity.kt
@@ -3,13 +3,11 @@ package com.tarikyasar.otpfield
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.tarikyasar.composeotpfield.OtpComposable
 import com.tarikyasar.otpfield.ui.theme.OtpFieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +15,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OtpFieldTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    OtpComposable(
+                        isOtpValid = {
+                            println(it)
+                        },
+                        codeLength = 6
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OtpFieldTheme {
-        Greeting("Android")
     }
 }
