da7f75d8e2a967e938bec4cfc6b308af67a253b4
mateuszkowol <mateusz.kowol@euvic.pl>
2024-09-25 13:59:28 +0200

    fix cell background

--- sample/src/main/java/com/composeuisuite/ohteepee/MainActivity.kt
+++ sample/src/main/java/com/composeuisuite/ohteepee/MainActivity.kt
@@ -3,7 +3,6 @@ package com.composeuisuite.ohteepee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -26,7 +25,7 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -53,7 +52,6 @@ import com.composeuisuite.ohteepee.configuration.OhTeePeeErrorAnimationConfig
 import com.composeuisuite.ohteepee.ui.theme.OtpFieldTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -386,7 +384,7 @@ private fun Sample2(
 
             Spacer(modifier = Modifier.width(16.dp))
 
-            Image(imageVector = Icons.Default.ArrowForward, contentDescription = "")
+            Image(imageVector = Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "")
         }
 
         Spacer(modifier = Modifier.height(64.dp))
